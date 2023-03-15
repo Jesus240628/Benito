6c441af699fce7055b40561d6994b738421b9be2
Jesus240628 <jesus.quintero240628@potros.itson.edu.mx>
2023-03-15 11:26:56 -0700

    Se creo metodo editar

--- src/mx/itson/benito/persistencia/ArticuloDAO.java
+++ src/mx/itson/benito/persistencia/ArticuloDAO.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.criteria.CriteriaQuery;
 import mx.itson.benito.entidades.Articulo;
+import mx.itson.benito.entidades.Proveedor;
 import mx.itson.benito.utilerias.HibernateUtil;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -18,6 +19,7 @@ import org.hibernate.Session;
  * @author Jesus Javier Quintero Fierro
  */
 public class ArticuloDAO {
+    
     public static List<Articulo> obtenerTodos(){
         List<Articulo> articulos = new ArrayList<>();
         try{
@@ -30,6 +32,7 @@ public class ArticuloDAO {
         }
     return articulos;
     }
+    
     public static boolean eliminar(int id){
     boolean resultado = false;
         try{
@@ -46,4 +49,26 @@ public class ArticuloDAO {
         }
         return resultado;
     }
+    
+    public static boolean editar(int id, String nombre, String clave, int precio, Proveedor proveedor){
+    boolean resultado = false;
+    try{
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        Articulo articulo = session.get(Articulo.class, id);
+        if(articulo != null){
+            articulo.setNombre(nombre);
+            articulo.setClave(clave);
+            articulo.setPrecio(precio);
+            articulo.setProveedor(proveedor);
+            session.saveOrUpdate(articulo);
+            session.getTransaction().commit();
+            resultado = true;
+            }       
+        }catch(HibernateException ex){
+            System.err.println("Ocurrio un error: " + ex.getMessage());
+        }
+        return resultado;
+    }
+    
 }
